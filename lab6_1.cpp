34623eb13d6dd801a65a3feddabbe0c71848518b
TitleRKD <sirawutpa@gmail.com>
2024-11-29 16:48:51 +0700

    lab6_1.cpp completed

--- lab6_1.cpp
+++ lab6_1.cpp
@@ -2,8 +2,25 @@
 using namespace std;
 
 int main(){
-    cout << "Enter an integer: ";
-    cout << "#Even numbers = ";
-    cout << "#Odd numbers = ";
+    int even=0;
+    int odd=0;
+    int number=0;
+    while(true){
+        cout << "Enter an integer: ";
+        cin >> number;
+        if(number == 0){
+            break;
+        }
+        else{
+            if(number%2 == 0){
+                even++;
+            }else
+                {
+                odd++;
+                }
+        }
+    }
+    cout << "#Even numbers = " << even << endl;
+    cout << "#Odd numbers = " << odd;
     return 0;
 }
